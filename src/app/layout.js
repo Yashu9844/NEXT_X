d384185fcae871d37f7a73f628cfe154d43d469f
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-08-19 13:46:40 +0530

    Update News Component with api call

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,7 +17,12 @@ export default function RootLayout({ children }) {
         <div className="flex justify-between max-w-6xl mx-auto">
               <div className="hidden sm:inline  border-r h-screen"><Sidebar/></div>
               <div  >   {children}</div>
-              <div><News/></div>
+              <div className="lg:flex-col p-3  h-screen border-l lg:flex w-[24rem] hidden">
+                <div className="py-2 bg-white sticky top-0">
+                  <input className="bg-gray-100 border border-gray-200 w-full rounded-3xl px-4 py-2 text-sm" type="text" placeholder="Search.."></input>
+                </div>
+                
+                <News/></div>
 
         </div>
      </body>
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+"use client";
+import React, { useEffect, useState } from 'react'
 
 const News = () => {
+   const [news,setNews] = useState([]);
+   const [newCount,setNewsCount]= useState(3);
+
+  useEffect(()=>{
+    fetch("https://saurav.tech/NewsAPI/top-headlines/category/business/in.json").then((res)=>res.json()).then((data)=>setNews(data.articles))
+  },[])
   return (
-    <div>
-      News
+    <div className='text-gray-700 bg-gray-100 rounded-lg pt-2 px-2 space-y-5'>
+      <h1 className='font-bold text-xl px-4'>Whats Happening</h1>
+      {news.slice(0,newCount).map((article,i)=>(
+        <div key={i}>
+         <a href={article.url} target='_blank'>
+          <div className="flex items-center space-x-1 px-4 py-2 hover:bg-gray-200 transition-all duration-200 justify-between">
+            <div className="space-y-0.5">
+              <h6 className='text-sm font-bold'>{article.title}</h6>
+              <p className='text-xs font-medium text-gray-500'>{article.source.name}</p>
+            </div>
+            <img src={article.urlToImage} alt="image" width={70} className='rounded-md' />
+          </div>
+         </a>
+        </div>
+      ))}
+    <div className="flex items-center justify-between ">
+    <button onClick={()=>setNewsCount(newCount+3)}
+     className='text-sm px-4 text-blue-400 pb-2 hover:text-blue-200 transition-all duration-200'
+    >
+          Load More  
+    </button>
+{newCount > 3 ? (
+   <button onClick={()=>setNewsCount(newCount-3)}
+   className='text-sm px-4 text-red-500 pb-2 hover:text-red-100 transition-all duration-200'
+  >
+        Load Less  
+  </button>
+):""}
+
+    </div>
+
+
     </div>
   )
 }
