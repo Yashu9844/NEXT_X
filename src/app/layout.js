ab09d69df57039be10ff3819b69acaebeb3e95bc
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-08-25 10:49:02 +0530

    Add input component and update home page

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,7 +18,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <div className="flex justify-between max-w-6xl mx-auto">
               <div className="hidden sm:inline  border-r h-screen"><Sidebar/></div>
-              <div  >   {children}</div>
+              <div className="w-2xl flex-1" >   {children}</div>
               <div className="lg:flex-col p-3  h-screen border-l lg:flex w-[24rem] hidden">
                 <div className="py-2 bg-white sticky top-0">
                   <input className="bg-gray-100 border border-gray-200 w-full rounded-3xl px-4 py-2 text-sm" type="text" placeholder="Search.."></input>
--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,12 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className=''>
+    <div className='max-w-xl mx-auto border-r border-l min-h-screen'>
       <div className='py-2 px-3 sticky z-50 bg-white border-b border-gray-200'>
        <h2 className='text-lg sm:text-xl font-bold' >Home</h2> 
-       <Input/> 
+       
       </div>
+      <Input/> 
     </div>
   )
 }
